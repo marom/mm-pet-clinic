91495de449be2a34485dc5ce07732c520e44570a
MaroM <marom@users.noreply.github.com>
2019-01-08 11:07:39 +0100

    Initial commit

---
+++ src/main/java/com/marom/mmpetclinic/MmPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.marom.mmpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmPetClinicApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/marom/mmpetclinic/MmPetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package com.marom.mmpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MmPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
