8100df70afbceaee27abfa72047736812aa3b8b0
MaroM <maro.muszynski@gmail.com>
2019-02-21 13:34:44 +0100

    Update Map Implementation to manage setting of ID property. closes #22.

--- pet-clinic-web/src/main/java/com/marom/mmpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/marom/mmpetclinic/bootstrap/DataLoader.java
@@ -22,32 +22,25 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Maro");
         owner1.setLastName("Kowalski");
-
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Kazimierz");
         owner2.setLastName("Burczymucha");
-
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Janek");
         vet1.setLastName("Wiewiora");
-
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Bogdan");
         vet2.setLastName("Kozihelper");
-
         vetService.save(vet2);
+
         System.out.println("All data are loaded.....");
 
     }
