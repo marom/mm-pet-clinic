7deaea5075499d07c046b246340b5f420a43df75
MaroM <maro.muszynski@gmail.com>
2019-02-23 18:34:55 +0100

    Menu links are broken closes #44

---
+++ pet-clinic-web/src/main/java/com/marom/mmpetclinic/controllers/ErrorController.java
@@ -0,0 +1,13 @@
+package com.marom.mmpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController {
+
+    @RequestMapping("/oups")
+    public String oupsError() {
+        return "notimplemented";
+    }
+}
