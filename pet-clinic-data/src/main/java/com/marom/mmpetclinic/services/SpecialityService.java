79a9117e0e7744f95dc6efeb2590a7ec7a980215
MaroM <maro.muszynski@gmail.com>
2019-02-23 17:44:46 +0100

    Fix naming, should be singular form

--- pet-clinic-data/src/main/java/com/marom/mmpetclinic/services/SpecialityService.java
+++ pet-clinic-data/src/main/java/com/marom/mmpetclinic/services/SpecialityService.java
@@ -2,5 +2,5 @@ package com.marom.mmpetclinic.services;
 
 import com.marom.mmpetclinic.model.Speciality;
 
-public interface SpecialitiesService extends CrudService<Speciality, Long> {
+public interface SpecialityService extends CrudService<Speciality, Long> {
 }
