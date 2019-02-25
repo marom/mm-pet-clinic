b75fcbcbaa01261a5dade90e557ad78231bf899c
MaroM <maro.muszynski@gmail.com>
2019-02-25 17:58:17 +0100

    Create Spring Data JPA service for Visit closes #46

---
+++ pet-clinic-data/src/main/java/com/marom/mmpetclinic/services/springdatajpa/VisitJpaService.java
@@ -0,0 +1,48 @@
+package com.marom.mmpetclinic.services.springdatajpa;
+
+import com.marom.mmpetclinic.model.Visit;
+import com.marom.mmpetclinic.repositories.VisitRepository;
+import com.marom.mmpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajap")
+public class VisitJpaService implements VisitService {
+
+    private VisitRepository visitRepository;
+
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
--- pet-clinic-web/src/main/java/com/marom/mmpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/marom/mmpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.marom.mmpetclinic.bootstrap;
 
 import com.marom.mmpetclinic.model.*;
-import com.marom.mmpetclinic.services.OwnerService;
-import com.marom.mmpetclinic.services.PetTypeService;
-import com.marom.mmpetclinic.services.SpecialtyService;
-import com.marom.mmpetclinic.services.VetService;
+import com.marom.mmpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -88,6 +87,13 @@ public class DataLoader implements CommandLineRunner {
         kaziksPet.setBirthDate(LocalDate.now());
         owner2.getPets().add(kaziksPet);
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(kaziksPet);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Janek");
