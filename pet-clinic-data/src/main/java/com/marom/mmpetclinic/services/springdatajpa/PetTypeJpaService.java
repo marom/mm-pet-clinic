c33b597d4b0b661c09ceda5d775e040eac275dfb
MaroM <maro.muszynski@gmail.com>
2019-02-25 13:58:54 +0100

    Create Spring Data JPA service for PetType closes #41

---
+++ pet-clinic-data/src/main/java/com/marom/mmpetclinic/services/springdatajpa/PetTypeJpaService.java
@@ -0,0 +1,49 @@
+package com.marom.mmpetclinic.services.springdatajpa;
+
+import com.marom.mmpetclinic.model.PetType;
+import com.marom.mmpetclinic.repositories.PetTypeRepository;
+import com.marom.mmpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Service
+@Profile("springdatajap")
+public class PetTypeJpaService implements PetTypeService {
+
+    private PetTypeRepository petTypeRepository;
+
+    public PetTypeJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
