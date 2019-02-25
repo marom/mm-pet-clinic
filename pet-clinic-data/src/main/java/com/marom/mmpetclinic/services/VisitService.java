8d0d7f87be92effef282c6fbd6c426478cf8fa32
MaroM <maro.muszynski@gmail.com>
2019-02-25 17:39:17 +0100

    Create Visit Map Service implementation closes #45

---
+++ pet-clinic-data/src/main/java/com/marom/mmpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.marom.mmpetclinic.services;
+
+import com.marom.mmpetclinic.model.Visit;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
