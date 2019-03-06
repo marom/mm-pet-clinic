1838406cf3dc6966dbdb7b9d28a0d5306fe876a1
MaroM <maro.muszynski@gmail.com>
2019-03-06 14:37:20 +0100

    create Pet controller and add model attributes for a pet types and an owner #54

---
+++ pet-clinic-web/src/main/java/com/marom/mmpetclinic/controllers/PetController.java
@@ -0,0 +1,42 @@
+package com.marom.mmpetclinic.controllers;
+
+import com.marom.mmpetclinic.model.Owner;
+import com.marom.mmpetclinic.model.PetType;
+import com.marom.mmpetclinic.services.OwnerService;
+import com.marom.mmpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(OwnerService ownerService, PetTypeService petTypeService) {
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("petTypes")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
