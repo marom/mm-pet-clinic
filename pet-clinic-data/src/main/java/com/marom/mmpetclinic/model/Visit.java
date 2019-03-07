0742e7615129e6127c0342b445c6d96882d76823
MaroM <maro.muszynski@gmail.com>
2019-03-07 12:23:55 +0100

    add builder for Visit #66

--- pet-clinic-data/src/main/java/com/marom/mmpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/marom/mmpetclinic/model/Visit.java
@@ -12,10 +12,18 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity {
+
+    @Builder
+    public Visit(Long id, LocalDate date, String description, Pet pet) {
+        super(id);
+        this.date = date;
+        this.description = description;
+        this.pet = pet;
+    }
+
 
     private LocalDate date;
     private String description;
